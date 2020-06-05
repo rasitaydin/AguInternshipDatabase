a402ded9077eac2894834b6a5710e293a5217fb0
Enes Kaçan <enes.kacan@agu.edu.tr>
2020-06-06 02:19:21 +0300

    main model injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:internshipdatabase/pages/add_internship_page.dart';
-import 'package:internshipdatabase/pages/internship_details_page.dart';
-import 'package:internshipdatabase/pages/login_page.dart';
-import 'package:internshipdatabase/pages/profile_page.dart';
-import 'package:internshipdatabase/pages/search_internship_page.dart';
-import 'package:internshipdatabase/pages/signup_page.dart';
-import 'package:internshipdatabase/pages/welcome_page.dart';
+import 'package:internshipdatabase/locator.dart';
+import 'package:internshipdatabase/viewmodels/main_model.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/services.dart' ;
+import 'pages/welcome_page.dart';
 
 void main() {
+  setupLocator();
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
-    return  MaterialApp(
+    return ChangeNotifierProvider(
+        create: (context) => MainModel(),
+        child: MaterialApp(
           theme: ThemeData(
             primarySwatch: Colors.grey,
             accentColor: Colors.grey,
           ),
           debugShowCheckedModeBanner: false,
           routes: {
-            '/'               : (context) => InternshipDetailsPage(),
-
+            '/'               : (context) => WelcomePage(),
           },
-        );
+        ));
   }
 }
