11ac5c28841bf2ce60ccefb3fed46f3d4ed9d8d9
Enes Kaçan <enes.kacan@agu.edu.tr>
2020-06-06 02:01:45 +0300

    main repository added

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,9 +1,11 @@
 import 'package:get_it/get_it.dart';
+import 'package:internshipdatabase/repository/main_repository.dart';
 import 'package:internshipdatabase/services/local_db_service.dart';
 
 GetIt locator = GetIt.instance;
 
 void setupLocator(){
   locator.registerLazySingleton(() => LocalDBService());
+  locator.registerLazySingleton(() => MainRepository());
 
 }
